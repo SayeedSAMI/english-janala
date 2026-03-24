978486f96426cdaf25b5e72ddbd882fcc727b48c
SayeedSami <sayeedsami26@gmail.com>
2026-03-25 01:46:38 +0600

    lessons more info modal added

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,13 @@ const loadLevel = () => {
     .then((response) => response.json())
     .then((data) => displayLevel(data.data));
 };
+const removeActive = () => {
+  const lessonBtn = document.querySelectorAll(".nonActive");
+  // console.log(lessonBtn);
+  lessonBtn.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
 
 const loadLesson = (level) => {
   const url = `https://openapi.programming-hero.com/api/level/${level}`;
@@ -12,6 +19,11 @@ const loadLesson = (level) => {
     .then((response) => response.json())
     .then((lessons) => {
       console.log(lessons.data);
+      removeActive();
+      const clickBtn = document.getElementById(`lesson-btn-${level}`);
+
+      clickBtn.classList.add("active");
+
       displayLessons(lessons.data);
     });
 };
@@ -36,13 +48,13 @@ const displayLessons = (lessons) => {
     const newLesson = document.createElement("div");
     newLesson.innerHTML = `
      <div class="max-w-xl text-center bg-[#ffffff] rounded-md p-10 items-center space-y-7 mx-auto">
-            <h1 class="text-3xl font-bold">${lesson.word ? "word not found" : lesson.word}</h1>
+            <h1 class="text-3xl font-bold">${lesson.word ? lesson.word : "word not found"}</h1>
             <p class="text-[20px] font-medium">Meaning /Pronounciation</p>
             <h1 class="font-bangla text-4xl font-semibold text-[#18181B] opacity-80">
             ${lesson.meaning ? lesson.meaning : "meaning not found"}
             /${lesson.pronunciation ? lesson.pronunciation : "pronunciation not found"}</h1>
             <div class="flex justify-between">
-              <button class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+              <button onclick="loadWordDetail(${lesson.id})" class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
               <button class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
      </div>
@@ -62,13 +74,57 @@ const displayLevel = (levels) => {
 
     const levelDataContainer = document.createElement("div");
     levelDataContainer.innerHTML = `
-    <button onclick="loadLesson(${level.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson -${level.level_no}</button>
+    <button id="lesson-btn-${level.level_no}" onclick="loadLesson(${level.level_no})" class="btn btn-outline btn-primary nonActive"><i class="fa-solid fa-book-open"></i> Lesson -${level.level_no}</button>
     `;
 
     // append the created div
 
     levelContainer.appendChild(levelDataContainer);
   });
+};
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+  const res = await fetch(url);
+  const details = await res.json();
+  console.log(details);
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (data) => {
+  const detailsBox = document.getElementById("detailsContainer");
+  detailsBox.innerHTML = `    
+  <h1 class="font-semibold text-4xl">${data.word}(<i class="fa-solid fa-microphone-lines"></i> ${data.pronunciation})</h1>
+            <div>
+              <h3 class="text-2xl font-semibold">Meaning</h3>
+              <p class="font-bangla py-4 text-2xl font-medium">${data.meaning}</p>
+            </div>
+            <div>
+              <h3 class="text-2xl font-semibold">Example</h3>
+              <p class="text-2xl font-normal">
+                ${data.sentence}
+              </p>
+            </div>
+
+            <h3 class="font-bangla text-2xl font-semibold">সমার্থক শব্দ গুলো</h3>
+            <div class="flex gap-3">
+              <button
+                class="bg-[#EDF7FF] border-[#D7E4EF] px-5 py-4 rounded-lg opacity-80"
+              >
+${data.synonyms[0]}</button
+              ><button
+                class="bg-[#EDF7FF] border-[#D7E4EF] px-5 py-4 rounded-lg opacity-80"
+              >
+               ${data.synonyms[1]}</button
+              ><button
+                class="bg-[#EDF7FF] border-[#D7E4EF] px-5 py-4 rounded-lg opacity-80"
+              >
+              ${data.synonyms[2]}
+              </button>
+            </div>`;
+
+  document.getElementById("my_modal_5").showModal();
 };
 
 loadLevel();
